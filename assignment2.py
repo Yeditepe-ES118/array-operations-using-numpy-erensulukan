e0261553f9d94699a4acb191ee5d0f56b378b57d
erensulukan <eren.sulukan@std.yeditepe.edu.tr>
2026-01-05 20:51:47 +0300

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,23 @@
+import numpy as np
+def stat (data):
+    #importing data
+    data = np.loadtxt ("populations.txt", skiprowns = 1)
+    
+    #Createing hare
+    hare = data[:,1]
+    
+    #Finding the min year of hare with np.argmin
+    min_hare_index = np.argmin(hare)
+    
+    min_year_hare = data(min_hare_index, 0)
+    
+    lynx_avg = np.mean(data[:, 2])
+    
+    species_sum = np.sum(data[:, 1:], axis=1)
+    
+    new_data = np.column_stack((data, species_sum))
+    
+    mask = new_data[:, 3] < 40000
+    new_data[mask, 3] = 0
+    
+    return data, hare, min_year_hare, lynx_avg, new_data
